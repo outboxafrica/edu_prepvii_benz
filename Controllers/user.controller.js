8978a40b6e067c8184dbdb83c7e902ba2d6ecd82
Kally <kallyasmedia@gmail.com>
2020-09-02 16:26:28 +0000

    Update User Model

---
+++ Controllers/user.controller.js
@@ -0,0 +1,1 @@
+// logic for user
--- Routes/user.route.js
+++ Routes/user.route.js
@@ -1,1 +1,26 @@
 // user Routes
+const express = require("express");
+const router = express.Router();
+
+// test route
+router.get("/test", (req, res) => {
+  res.send(`Hello from test Route`);
+});
+
+router.get("/", (req, res) => {
+  res.json({
+    name: "edu api",
+    version: "1.0.0",
+    author: "team-benz",
+  });
+});
+
+//create User
+
+//delete user by Id
+
+//update User by Id
+
+// view user
+
+module.exports = router;
