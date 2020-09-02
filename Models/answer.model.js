7aab14ab15610da7407c3060f7b660fcd35d536f
Kally <kallyasmedia@gmail.com>
2020-09-02 16:05:50 +0000

    Add Answer schema

--- Models/answer.model.js
+++ Models/answer.model.js
@@ -1,1 +1,20 @@
 // Answer Model
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AnswerSchema = new Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  answer: {
+    type: Text,
+    required: true,
+    max: 1000,
+  },
+  upVote: {
+    type: Number,
+  },
+  downVote: {
+    type: Number,
+  },
+});
+
+module.exports = mongoose.model("answer", AnswerSchema);
