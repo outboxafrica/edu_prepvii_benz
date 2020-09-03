e3f054b171d7231e1d084d38533dfca7f69f86e8
Kally <kallyasmedia@gmail.com>
2020-09-03 15:11:21 +0000

    add email validation@

--- Models/user.model.js
+++ Models/user.model.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
+// email validation credit: https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax/28396238
+let validateEmail = (email) => {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
 // define User schema
 const UserSchema = new Schema({
   //   _id: mongoose.Schema.Types.ObjectId,
@@ -12,8 +17,15 @@ const UserSchema = new Schema({
   },
   email: {
     type: String,
+    trim: true,
+    lowercase: true,
     unique: true,
-    required: true,
+    required: "Email address is required",
+    validate: [validateEmail, "Please fill a valid email address"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
   },
   password: {
     type: String,
@@ -23,6 +35,12 @@ const UserSchema = new Schema({
     },
   },
   contact: Number,
+  answers: {
+    type: Array,
+  },
+  questions: {
+    type: Array,
+  },
 });
 
 module.exports = mongoose.model("User", UserSchema);
