4598dcb629abb1a4c4265c903f149db88389cb7d
Kally <kallyasmedia@gmail.com>
2020-09-03 15:24:02 +0000

    Update question schema

--- Models/question.model.js
+++ Models/question.model.js
@@ -9,6 +9,22 @@ const questionSchema = new Schema({
     required: true,
     max: 1000,
   },
+  // https://stackoverflow.com/questions/18001478/referencing-another-schema-in-mongoose
+  // postedBy: userSchema referencing the whole user schema
+  postedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+  comments: [
+    {
+      body: "string",
+      by: mongoose.Schema.Types.ObjectId,
+    },
+  ],
   upVote: {
     type: Number,
   },
