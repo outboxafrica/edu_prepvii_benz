6f5081e2e99fbaa4f3b01cb659a5688447f6b42c
Kally <kallyasmedia@gmail.com>
2020-09-04 13:12:00 +0000

    Restructure code

--- Routes/answer.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const answerController = require("../Controllers/answer.controller");
-
-router.post("/answer/create", answerController.createAns);
-
-module.exports = router;
