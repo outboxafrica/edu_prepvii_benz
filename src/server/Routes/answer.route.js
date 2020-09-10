4c995059021270adaede8e9cecce6771b30f5996
Kally <60466044+kallyas@users.noreply.github.com>
2020-09-10 16:36:01 +0300

    Update answer.route.js

--- src/server/Routes/answer.route.js
+++ src/server/Routes/answer.route.js
@@ -4,18 +4,18 @@ const router = express.Router();
 const answerController = require("../Controllers/answer.controller");
 
 //post an answer
-router.post("/answer/create", answerController.createAns);
+router.post("/questions/:id/answer", answerController.createAns);
 
 // get all answers
 router.get("/answers", answerController.getAns);
 
 // update answer
-router.put("/answer/:id/update", answerController.updateAns);
+router.put("/questions/:id/answer/:id", answerController.updateAns);
 
 // delete answer
-router.delete("/answer/:id/delete", answerController.deleteAns);
+router.delete("/questions/:id/answer/:id", answerController.deleteAns);
 
 // get answer by Id
-router.get('/answer/:id', answerController.getAnsById)
+router.get("/questions/:id/answer/:id", answerController.getAnsById);
 
 module.exports = router;
