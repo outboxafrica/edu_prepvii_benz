f64be564c3066d62d1d6301b7587b3b2a2b4ca73
Kally <60466044+kallyas@users.noreply.github.com>
2020-09-11 12:47:15 +0300

    Update user.controller.js

--- src/server/Controllers/user.controller.js
+++ src/server/Controllers/user.controller.js
@@ -1,1 +1,47 @@
 // User Model
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const Schema = mongoose.Schema;
+
+// email validation credit: https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax/28396238
+let validateEmail = (email) => {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+// define User schema
+const UserSchema = new Schema({
+  //   _id: mongoose.Schema.Types.ObjectId,
+  username: {
+    type: String,
+    required: true,
+    max: 100,
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: "Email address is required",
+    validate: [validateEmail, "Please fill a valid email address"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+  password: {
+    type: String,
+    required: true,
+    set: (value) => {
+      return bcrypt.hashSync(value, 10);
+    },
+  },
+  contact: Number,
+  answers: {
+    type: Array,
+  },
+  questions: {
+    type: Array,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
