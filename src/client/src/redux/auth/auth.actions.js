ef2137ae8c621efa437a0eb6caac5ddf01b9bb9b
Kally <kallyasmedia@gmail.com>
2020-10-14 13:50:52 +0000

    ch: set up user auth

---
+++ src/client/src/redux/auth/auth.actions.js
@@ -0,0 +1,101 @@
+import axios from 'axios';
+import { setAlert } from '../alert/alert.actions';
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT
+} from './auth.types';
+
+import setAuthToken from './auth.utils';
+
+const API = `https://api-team-benz.herokuapp.com/api/v1`
+
+// Load User
+// export const loadUser = () => async dispatch => {
+//     if (localStorage.token){
+//         setAuthToken(localStorage.token);
+//     }
+//     try {
+//         const res = await axios.get(`${API}`/auth);
+
+//         dispatch({
+//             type: USER_LOADED,
+//             payload: res.data.data
+//         });
+//     } catch (err) {
+//         dispatch({
+//             type: AUTH_ERROR
+//         });
+//     }
+// };
+
+// Register User
+export const register = ({ username, password }) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const body = JSON.stringify({ username, password });
+
+    try {
+        const res = await axios.post(`${API}/auth/signup`, body, config);
+
+        dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data.data
+        });
+
+        dispatch(setAlert(res.data.message, 'success'));
+
+        dispatch(loadUser());
+    } catch (err) {
+        dispatch(setAlert(err.response.data.message, 'danger'));
+
+        dispatch({
+            type: REGISTER_FAIL
+        });
+    }
+};
+
+// Login User
+export const login = ({ username, password }) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const body = JSON.stringify({ username, password });
+
+    try {
+        const res = await axios.post(`${API}/auth/login`, body, config);
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data.data
+        });
+
+        dispatch(setAlert(res.data.message, 'success'));
+
+        dispatch(loadUser());
+    } catch (err) {
+        dispatch(setAlert(err.response.data.message, 'danger'));
+
+        dispatch({
+            type: LOGIN_FAIL
+        });
+    }
+};
+
+//LOGOUT
+export const logout = () => dispatch => {
+    dispatch(setAlert('User has logged out', 'success'));
+
+    dispatch({ type: LOGOUT });
+};
