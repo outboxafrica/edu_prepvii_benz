0345204540a67524ed9be25de36304192107a09c
Kally <kallyasmedia@gmail.com>
2020-10-14 13:00:30 +0000

    ch: add user actions

--- src/client/src/redux/users/users.actions.js
+++ src/client/src/redux/users/users.actions.js
@@ -1,2 +1,40 @@
 import { GET_USERS, GET_USER, USER_ERROR } from './users.types'
 import axios from 'axios'
+
+
+const API = `https://api-team-benz.herokuapp.com/api/v1/`
+// get users
+export const getUsers = () => async dispatch => {
+    try {
+        const res = axios.get(`${API}/users`)
+        dispatch({
+            type: GET_USERS,
+            payload: res.data.data
+        })
+    } catch (error) {
+         dispatch({
+            type: USER_ERROR,
+            payload: { 
+                msg: err.response.statusText, 
+                status: err.response.status 
+                }
+        });
+    }
+}
+
+// Get user
+export const getUser = id => async dispatch => {
+    try {
+        const res = await axios.get(`${API}/users/${id}`);
+
+        dispatch({
+            type: GET_USER,
+            payload: res.data.data
+        });
+    } catch (err) {
+        dispatch({
+            type: USER_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
